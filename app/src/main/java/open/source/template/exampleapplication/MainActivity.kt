253a0c5cd58c7994fc91f6ada783d7dd2afac6ec
Anton Saatze <32552797+Tosaa@users.noreply.github.com>
2021-03-02 17:30:28 +0100

    Initial commit

---
+++ app/src/main/java/open/source/template/exampleapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package open.source.template.exampleapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
